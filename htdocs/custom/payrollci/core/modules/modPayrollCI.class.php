09da9691e071b4ae6ec5520d545cd5a3ff25d0c4
Eudes8 <86252879+Eudes8@users.noreply.github.com>
2026-04-15 14:45:34 +0000

    [PayrollCI v3] Add core/modules/modPayrollCI.class.php

--- htdocs/custom/payrollci/core/modules/modPayrollCI.class.php
+++ htdocs/custom/payrollci/core/modules/modPayrollCI.class.php
@@ -1,5 +1,7 @@
 <?php
-/* PayrollCI v2 - Module descriptor */
+/* ============================================================================
+ * PayrollCI v3 - Module descriptor - Intégration complète Dolibarr
+ * ============================================================================ */
 
 include_once DOL_DOCUMENT_ROOT.'/core/modules/DolibarrModules.class.php';
 
@@ -14,56 +16,110 @@ class modPayrollCI extends DolibarrModules
         $this->family = "hr";
         $this->module_position = '90';
         $this->name = preg_replace('/^mod/i', '', get_class($this));
-        $this->description = "Gestion complète de la paie - Droit ivoirien 2026 (v2)";
-        $this->descriptionlong = "Module de paie conforme au droit du travail de Côte d'Ivoire. Inclut tous les éléments CCI, charges CNPS, ITS, charges fiscales patronales (IE, FDFP), avantages en nature, heures supplémentaires (15/50/75/100%), transport exonéré, prime d'ancienneté auto.";
+        $this->description = "Gestion complète de la paie - Droit ivoirien 2026 (v3)";
+        $this->descriptionlong = "Module de paie 100% intégré à Dolibarr, conforme au droit du travail de Côte d'Ivoire. Inclut tous les éléments CCI, charges CNPS, ITS, charges fiscales patronales (IE, FDFP), avantages en nature, heures supplémentaires, transport exonéré, ancienneté auto. Onglet paie sur les fiches utilisateurs/employés.";
         $this->editor_name = 'PayrollCI';
-        $this->version = '2.0.0';
+        $this->version = '3.0.0';
         $this->const_name = 'MAIN_MODULE_'.strtoupper($this->name);
         $this->picto = 'payrollci@payrollci';
 
-        $this->module_parts = array('triggers' => 0);
+        // Module parts
+        $this->module_parts = array(
+            'triggers' => 1,
+            'login'    => 0,
+            'substitutions' => 0,
+            'menus'    => 0,
+            'tpl'      => 0,
+            'barcode'  => 0,
+            'models'   => 1,
+            'theme'    => 0,
+            'css'      => array(),
+            'hooks'    => array(
+                'data' => array('usercard', 'globalcard'),
+                'entity' => '0',
+            ),
+            'moduleforexternal' => 0,
+        );
+
         $this->dirs = array('/payrollci/temp', '/payrollci/bulletins');
         $this->config_page_url = array("setup.php@payrollci");
 
-        $this->tabs = array();
+        // Onglet "Bulletins de paie" sur les fiches utilisateurs
+        $this->tabs = array(
+            'user:+payrollci:BulletinsDePaie:payrollci@payrollci:$conf->payrollci->enabled:/payrollci/tab_user.php?fk_user=__ID__',
+        );
+
+        // Dictionnaires
         $this->dictionaries = array();
-        $this->boxes = array();
+
+        // Boxes / Widgets
+        $this->boxes = array(
+            0 => array(
+                'file'    => 'box_payrollci_last@payrollci',
+                'note'    => 'Derniers bulletins de paie',
+                'enabledbydefaulton' => 'Home',
+            ),
+        );
+
+        // Cron jobs
         $this->cronjobs = array();
 
+        // Constantes
+        $this->const = array(
+            0 => array('PAYROLLCI_DEFAULT_SECTEUR', 'chaine', 'commerce', 'Secteur par défaut', 0, 'current', 1),
+            1 => array('PAYROLLCI_DEFAULT_VILLE', 'chaine', 'abidjan', 'Ville par défaut', 0, 'current', 1),
+        );
+
+        // Permissions
         $this->rights = array();
         $r = 0;
         $this->rights[$r][0] = 500001;
         $this->rights[$r][1] = 'Lire les bulletins de paie';
         $this->rights[$r][3] = 0;
-        $this->rights[$r][4] = 'read';
+        $this->rights[$r][4] = 'lire';
+        $this->rights[$r][5] = '';
         $r++;
         $this->rights[$r][0] = 500002;
         $this->rights[$r][1] = 'Créer/modifier les bulletins';
         $this->rights[$r][3] = 0;
-        $this->rights[$r][4] = 'write';
+        $this->rights[$r][4] = 'creer';
+        $this->rights[$r][5] = '';
         $r++;
         $this->rights[$r][0] = 500003;
         $this->rights[$r][1] = 'Supprimer les bulletins';
         $this->rights[$r][3] = 0;
-        $this->rights[$r][4] = 'delete';
+        $this->rights[$r][4] = 'supprimer';
+        $this->rights[$r][5] = '';
+        $r++;
+        $this->rights[$r][0] = 500004;
+        $this->rights[$r][1] = 'Exporter les bulletins';
+        $this->rights[$r][3] = 0;
+        $this->rights[$r][4] = 'export';
+        $this->rights[$r][5] = '';
 
+        // Menus
         $this->menu = array();
         $r = 0;
+
+        // Menu principal dans HRM
         $this->menu[$r] = array(
             'fk_menu'  => 'fk_mainmenu=hrm',
             'type'     => 'left',
-            'titre'    => 'Bulletins de paie',
+            'titre'    => 'Paie CI',
+            'prefix'   => img_picto('', 'payrollci@payrollci', 'class="paddingright"'),
             'mainmenu' => 'hrm',
             'leftmenu' => 'payrollci',
             'url'      => '/payrollci/list.php',
             'langs'    => 'payrollci@payrollci',
             'position' => 100,
             'enabled'  => '$conf->payrollci->enabled',
-            'perms'    => '$user->rights->payrollci->read',
+            'perms'    => '$user->rights->payrollci->lire',
             'target'   => '',
             'user'     => 0,
         );
         $r++;
+
+        // Sous-menu : Nouveau bulletin
         $this->menu[$r] = array(
             'fk_menu'  => 'fk_mainmenu=hrm,fk_leftmenu=payrollci',
             'type'     => 'left',
@@ -74,9 +130,11 @@ class modPayrollCI extends DolibarrModules
             'langs'    => 'payrollci@payrollci',
             'position' => 101,
             'enabled'  => '$conf->payrollci->enabled',
-            'perms'    => '$user->rights->payrollci->write',
+            'perms'    => '$user->rights->payrollci->creer',
         );
         $r++;
+
+        // Sous-menu : Liste des bulletins
         $this->menu[$r] = array(
             'fk_menu'  => 'fk_mainmenu=hrm,fk_leftmenu=payrollci',
             'type'     => 'left',
@@ -87,7 +145,37 @@ class modPayrollCI extends DolibarrModules
             'langs'    => 'payrollci@payrollci',
             'position' => 102,
             'enabled'  => '$conf->payrollci->enabled',
-            'perms'    => '$user->rights->payrollci->read',
+            'perms'    => '$user->rights->payrollci->lire',
+        );
+        $r++;
+
+        // Sous-menu : Tableau de bord
+        $this->menu[$r] = array(
+            'fk_menu'  => 'fk_mainmenu=hrm,fk_leftmenu=payrollci',
+            'type'     => 'left',
+            'titre'    => 'Tableau de bord',
+            'mainmenu' => 'hrm',
+            'leftmenu' => 'payrollci_dashboard',
+            'url'      => '/payrollci/dashboard.php',
+            'langs'    => 'payrollci@payrollci',
+            'position' => 103,
+            'enabled'  => '$conf->payrollci->enabled',
+            'perms'    => '$user->rights->payrollci->lire',
+        );
+        $r++;
+
+        // Sous-menu : Configuration (admin seulement)
+        $this->menu[$r] = array(
+            'fk_menu'  => 'fk_mainmenu=hrm,fk_leftmenu=payrollci',
+            'type'     => 'left',
+            'titre'    => 'Configuration',
+            'mainmenu' => 'hrm',
+            'leftmenu' => 'payrollci_setup',
+            'url'      => '/payrollci/admin/setup.php',
+            'langs'    => 'payrollci@payrollci',
+            'position' => 110,
+            'enabled'  => '$conf->payrollci->enabled',
+            'perms'    => '$user->admin',
         );
     }
 
