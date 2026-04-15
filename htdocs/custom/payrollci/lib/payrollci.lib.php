0fe4b13306a245b7efcd255677a327e882a26fec
Eudes8 <86252879+Eudes8@users.noreply.github.com>
2026-04-15 12:32:09 +0000

    [PayrollCI v2] Ajout: payrollci.lib.php

--- htdocs/custom/payrollci/lib/payrollci.lib.php
+++ htdocs/custom/payrollci/lib/payrollci.lib.php
@@ -1,67 +1,24 @@
 <?php
-/* ============================================================================
- * PayrollCI - Fonctions bibliothèque
- * ============================================================================
- */
+/* PayrollCI v2 - Fonctions utilitaires */
 
-/**
- * Prépare les onglets de l'administration du module
- */
-function payrollciAdminPrepareHead()
-{
-    global $langs, $conf;
-
-    $h = 0;
-    $head = array();
-
-    $head[$h][0] = dol_buildpath("/payrollci/admin/setup.php", 1);
-    $head[$h][1] = 'Configuration';
-    $head[$h][2] = 'settings';
-    $h++;
-
-    complete_head_from_modules($conf, $langs, null, $head, $h, 'payrollci@payrollci');
-
-    return $head;
+function payrollci_format_amount($amount) {
+    if ($amount == 0) return '-';
+    return number_format($amount, 0, ',', ' ');
 }
 
-/**
- * Formate un montant en FCFA
- */
-function payrollci_format_amount($amount)
-{
-    return number_format(round($amount), 0, ',', ' ').' FCFA';
-}
-
-/**
- * Retourne les mois en français
- */
-function payrollci_get_mois()
-{
-    return array(
-        1  => 'Janvier',
-        2  => 'Février',
-        3  => 'Mars',
-        4  => 'Avril',
-        5  => 'Mai',
-        6  => 'Juin',
-        7  => 'Juillet',
-        8  => 'Août',
-        9  => 'Septembre',
-        10 => 'Octobre',
-        11 => 'Novembre',
-        12 => 'Décembre',
-    );
-}
-
-/**
- * Retourne les situations familiales
- */
-function payrollci_get_situations()
-{
-    return array(
+function payrollci_get_situations() {
+    return [
         'celibataire' => 'Célibataire',
         'marie'       => 'Marié(e)',
         'divorce'     => 'Divorcé(e)',
         'veuf'        => 'Veuf/Veuve',
-    );
+    ];
+}
+
+function payrollci_get_mois() {
+    return [
+        1 => 'Janvier', 2 => 'Février', 3 => 'Mars', 4 => 'Avril',
+        5 => 'Mai', 6 => 'Juin', 7 => 'Juillet', 8 => 'Août',
+        9 => 'Septembre', 10 => 'Octobre', 11 => 'Novembre', 12 => 'Décembre',
+    ];
 }
