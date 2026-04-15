9d7787f3d18506482bf90034f81bec43ee4f929d
Eudes8 <86252879+Eudes8@users.noreply.github.com>
2026-04-15 15:28:28 +0000

    feat(payrollci): V4 - llx_payrollci_payslip.key.sql

--- htdocs/custom/payrollci/sql/llx_payrollci_payslip.key.sql
+++ htdocs/custom/payrollci/sql/llx_payrollci_payslip.key.sql
@@ -1,10 +1,9 @@
--- ============================================================================
--- Module PayrollCI v2 - Index et contraintes
--- ============================================================================
-
+-- PayrollCI v4 - Indexes
 ALTER TABLE llx_payrollci_payslip ADD INDEX idx_payrollci_payslip_ref (ref);
-ALTER TABLE llx_payrollci_payslip ADD INDEX idx_payrollci_payslip_fk_user (fk_user);
-ALTER TABLE llx_payrollci_payslip ADD INDEX idx_payrollci_payslip_date (date_start, date_end);
-ALTER TABLE llx_payrollci_payslip ADD INDEX idx_payrollci_payslip_status (status);
 ALTER TABLE llx_payrollci_payslip ADD INDEX idx_payrollci_payslip_entity (entity);
-ALTER TABLE llx_payrollci_payslip ADD UNIQUE INDEX uk_payrollci_payslip_ref (ref, entity);
+ALTER TABLE llx_payrollci_payslip ADD INDEX idx_payrollci_payslip_fk_user (fk_user);
+ALTER TABLE llx_payrollci_payslip ADD INDEX idx_payrollci_payslip_date_start (date_start);
+ALTER TABLE llx_payrollci_payslip ADD INDEX idx_payrollci_payslip_status (status);
+ALTER TABLE llx_payrollci_payslip ADD INDEX idx_payrollci_payslip_date_embauche (date_embauche);
+ALTER TABLE llx_payrollci_payslip ADD INDEX idx_payrollci_payslip_fk_soc (fk_soc);
+ALTER TABLE llx_payrollci_payslip ADD INDEX idx_payrollci_payslip_fk_project (fk_project);
